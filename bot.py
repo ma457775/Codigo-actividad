2fea68fcf76d7ff2907aff8dded07863f0e730da
El mundo de Matías <matyalpo@gmail.com>
2025-01-29 19:20:33 -0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import random
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str): 
+    await ctx.send(random.choice(choices))
+
+
+
+bot.run(":)")     
